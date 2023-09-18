1f9b6984695c50dca5bb5bd6d63ab3fca397c176
JinZr <60612200+JinZr@users.noreply.github.com>
2023-09-18 14:30:23 +0800

    updated

---
+++ egs/aishell/ASR/train.sh
@@ -0,0 +1,13 @@
+./zipformer/train.py \
+    --world-size 2 \
+    --num-epochs 60 \
+    --use-fp16 1 \
+    --context-size 1 \
+    --max-duration 1000 \
+    --exp-dir ./zipformer/exp-0.3-init \
+    --enable-musan 0 \
+    --base-lr 0.045 \
+    --lr-batches 7500 \
+    --lr-epochs 18 \
+    --spec-aug-time-warp-factor 20 \
+    --mid-rnnt-loss-scale 0.3
