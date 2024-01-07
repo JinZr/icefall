7b47d7cf79741c4824ab1e8dc7ecdc6327d12ff8
jinzr <zengrui.jin0@gmail.com>
2024-01-07 15:47:48 +0800

    Update finetune.py

--- egs/aishell/ASR/zipformer/finetune.py
+++ egs/aishell/ASR/zipformer/finetune.py
@@ -1199,7 +1199,6 @@ def run(rank, world_size, args):
             model.parameters(),
             lr=params.base_lr,
             clipping_scale=2.0,
-            parameters_names=parameters_names,
         )
 
     scheduler = Eden(
