7bf603d3bdcb2652b6c774223fe3d779f1bce96b
jinzr <zengrui.jin0@gmail.com>
2024-04-10 23:57:18 +0800

    init commit

---
+++ egs/stutter/local/preprocess_stutter.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright    2024  Xiaomi Corp.        (authors: Zengrui Jin)
+#
+# See ../../../../LICENSE for clarification regarding multiple authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import argparse
+from pathlib import Path
+
+import jieba
+import paddle
+from tqdm import tqdm
+
+from icefall.utils import is_cjk
+
+paddle.enable_static()
+jieba.enable_paddle()
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="Preprocess transcript for stuttering speech challenge",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--kaldi-dir",
+        type=str,
+        help="the kaldi fmt directory for stuttering speech challenge",
+    )
+    return parser
+
+
+def read_file_dict(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+    file_dict = {}
+    for line in lines:
+        key, value = line.split(" ", maxsplit=1)
+        file_dict[key] = value
+    return file_dict
+
+
+def write_file_dict(file_dict, file_path):
+    with open(file_path, "w", encoding="utf-8") as f:
+        for key, value in file_dict.items():
+            f.write(f"{key} {value}\n")
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    kaldi_dir = Path(args.kaldi_dir)
+
+    input_text = kaldi_dir / "text"
+    text_preprocessed = kaldi_dir / "text.preprocessed"
+    text_segment = kaldi_dir / "text.segment"
+    utt2spk = kaldi_dir / "utt2spk"
+
+    text_dict = read_file_dict(input_text)
+
+    seg_lines = dict()
+    preprocessed_lines = dict()
+    for key, line in tqdm(text_dict.items(), desc="preprocessing text"):
+        new_line = line.strip().upper().remove(",")
+        seg_list = jieba.cut(new_line, use_paddle=True)
+        seg_line = " ".join(seg_list)
+
+        preprocessed_line = ""
+        last_is_cjk = False
+        for word in seg_list:
+            if is_cjk(word[0]):
+                if last_is_cjk:
+                    preprocessed_line += word
+                else:
+                    preprocessed_line += f" {word}"
+                last_is_cjk = True
+            else:
+                preprocessed_line += f" {word}"
+                last_is_cjk = False
+        preprocessed_lines[key] = new_line
+        seg_lines[key] = seg_line
+
+    write_file_dict(preprocessed_lines, text_preprocessed)
+
+    write_file_dict(seg_lines, text_segment)
+
+    utt2spk_lines = dict()
+    for key in preprocessed_lines.keys():
+        utt2spk_lines[key] = key
+
+    write_file_dict(utt2spk_lines, utt2spk)
