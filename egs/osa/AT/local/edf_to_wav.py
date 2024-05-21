450c61ab124b29ea9eeae918518a9d654081d124
jinzr <zengrui.jin0@gmail.com>
2024-05-21 23:13:41 +0800

    init commit

---
+++ egs/osa/AT/local/edf_to_wav.py
@@ -0,0 +1,50 @@
+import argparse
+import logging
+from pathlib import Path
+
+import soundfile
+from pyedflib.highlevel import read_edf
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-edf", type=str, required=True, help="Path to the edf file."
+    )
+    parser.add_argument(
+        "--output-wav", type=str, required=True, help="Path to the wav file."
+    )
+    return parser
+
+
+def edf_contents(edf_file: Path):
+    signals, signal_headers, header = read_edf(edf_file)
+    return signals, signal_headers, header
+
+
+def get_audio(signal_headers, signals):
+    audio_header = signal_headers[-4]
+    audio = signals[-4]
+    return audio_header, audio
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    input_edf = args.input_edf
+    output_wav = Path(args.output_wav)
+
+    signals, signal_headers, _ = edf_contents(input_edf)
+    audio_header, audio = get_audio(signal_headers, signals)
+
+    assert audio_header["label"] == "Audio", "Audio channel not matched"
+
+    physical_max = audio_header["physical_max"]
+
+    audio = audio / physical_max
+
+    soundfile.write(f"{output_wav}", audio, 8000)
+    logging.info(f"Saved {output_wav}")
---
+++ egs/osa/AT/local/format_supervision_xls.py
@@ -0,0 +1,90 @@
+import argparse
+import logging
+from pathlib import Path
+from statistics import mean
+
+from pandas import read_excel
+
+TARGET_EVENTS = ["低通气", "A. 阻塞性", "A. 中枢性", "A. 混合性"]
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "--input-xls", type=str, required=True, help="Path to the xls file."
+    )
+    parser.add_argument(
+        "--output-csv", type=str, required=True, help="Path to the output csv file."
+    )
+
+    return parser
+
+
+def get_rows(xls_file):
+    df = read_excel(xls_file)
+    return df.values
+
+
+def filter_rows(rows):
+    return [row for row in rows if row[-1] in TARGET_EVENTS]
+
+
+def to_list(rows):
+    if type(rows) == list:
+        return [row.tolist() for row in rows]
+    else:
+        return rows.tolist()
+
+
+def append_relative_timestamp(first_event, rows):
+    return [
+        [
+            row[0],  # Absolute timestamp
+            row[0] - first_event[0],  # Relative timestamp
+            row[1],  # Duration
+            row[2],  # Event
+        ]
+        for row in rows
+    ]
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    input_xls = Path(args.input_xls)
+    output_csv = Path(args.output_csv)
+
+    rows = get_rows(input_xls)
+    first_row, last_row = rows[1], rows[-1]
+    first_row = to_list(first_row)
+    last_row = to_list(last_row)
+    # The first row is the header processed automatically by pandas
+    # The second row is ``[]	[秒]	[]`` which is not useful
+
+    rows = filter_rows(rows)
+    rows = to_list(rows)
+    rows = append_relative_timestamp(first_row, rows)
+
+    with open(output_csv, "w") as fout:
+        for row in rows:
+            fout.write(",".join([str(x) for x in row]) + "\n")
+
+    logging.info(f"==> Info for {input_xls.name}:")
+    logging.info(f"Number of rows: {len(rows)}")
+    logging.info(f"Total duration of sleep: {last_row[0] - first_row[0]}s")
+    for event in TARGET_EVENTS:
+        logging.info(f"Name: {event}")
+        logging.info(f"Number: {len([row for row in rows if row[-1] == event])}")
+        if len([row for row in rows if row[-1] == event]) > 0:
+            logging.info(
+                f"Average duration: {mean([row[2] for row in rows if row[-1] == event])}s"
+            )
+            logging.info(
+                f"Total duration: {sum([row[2] for row in rows if row[-1] == event])}s"
+            )
