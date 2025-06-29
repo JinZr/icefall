b05d78ab094e6e2cd0f9d91b750bfce022778804
zr_jin <peter.jin.cn@gmail.com>
2025-06-29 15:42:33 +0800

    Update train_wavlm_ctc_base.sh

--- egs/pumch/ASR/train_wavlm_ctc_base.sh
+++ egs/pumch/ASR/train_wavlm_ctc_base.sh
@@ -4,5 +4,5 @@ CUDA_VISIBLE_DEVICES=0 python ./wavlm_ctc/finetune.py \
     --train-csv ./data/SHI_batch_1_seg/SHI_batch_1_seg.csv \
     --valid-csv ./data/SHI_batch_2_seg/SHI_batch_2_seg.csv \
     --model-name ./wavlm_ctc/wavlm-base/ \
-    --batch-size 32 \
+    --batch-size 64 \
     --exp-dir ./wavlm_ctc/exp_base/ 
